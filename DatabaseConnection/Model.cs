d832ff8d08e90e66b39de05d440244eb65efe95d
Daniel Persson <daniel.persson18@learnet.se>
2020-11-19 15:58:08 +0100

    Mest nya kommentarer om koden i Mainwindow, + lite generella kommentarer om "what to do" inför framtiden

--- DatabaseConnection/Model.cs
+++ DatabaseConnection/Model.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace DatabaseConnection
 {
-    public class Customer
+    public class Customer // WiP tror vi behöver mer kundinformation, kund id och namn känns lite...
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public virtual List<Rental> Sales { get; set; } // Ger tillåtelse för rental
     }
-    public class Movie
+    public class Movie // Movie är däremot ganska bra
     {
         public int Id { get; set; }
         public string Title { get; set; }
@@ -22,7 +22,7 @@ namespace DatabaseConnection
         public string Genre { get; set; }
         public virtual List<Rental> Sales { get; set; } // Ger tillåtelse för rental
     }
-    public class Rental
+    public class Rental // Ska lägga till en "Hyrfilmen slutar flik"
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
